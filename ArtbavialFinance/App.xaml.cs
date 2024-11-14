f073960931d6260e0b1dde7efc27a2d35db60193
artbavial <pipleworld@gmail.com>
2024-11-14 11:04:44 +0300

    Добавьте файлы проекта.

---
+++ ArtbavialFinance/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ArtbavialFinance
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ArtbavialFinance/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ArtbavialFinance
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
